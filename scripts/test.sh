7ffb73ef666435882678bf0a7867aa2846366b6e
Raymond Akornor <raymondakornor@gmail.com>
2019-06-17 12:25:01 +0000

    Add template.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,35 +4,16 @@ set -e
  
 #lint
 # (cd app && npm install --silent && npm run lint)
-=======
+ 
 if [ $NODE_ENV = "development" ]; then
-    export DATABASE__URL="mongodb://mongo:27017/mosey_user_api"
-    export JWT__SECRET_KEY="jwt_key_for_token_generation_in_shell" 
-    export WEBHOOK__MOSEY_FEED_API_URL="https://mosey-feed-api-test.herokuapp.com/api/v1/store_experience"
+    export DATABASE__URL="process.env.DATABASE__URL"
+    export JWT__SECRET_KEY="process.env.JWT__SECRET_KEY" 
+    export WEBHOOK__MOSEY_FEED_API_URL="process.env.WEBHOOK__MOSEY_FEED_API_URL"
     export AWS__BUCKET="ampersand-mosey-dev"
-    export AWS__SECRET_ACCESS_KEY="AKIAIK7OOIBJ73HRYJRA"
-    export AWS__BUCKET="ampersand-mosey-dev"
+    export AWS__SECRET_ACCESS_KEY="process.env.AWS__SECRET_ACCESS_KEY"
+    export AWS__BUCKET="process.env.AWS__BUCKET"
     export AWS__QUEUE_URL="com-ampersand-mosey-image-upload-queue"
     export AWS__REGION="us-west-1"
-    export TINYPNG__API_KEY="8l6wsRTYg85cWktSmbTZ1XCr9ktH9QNG"
+    export TINYPNG__API_KEY="process.env.TINYPNG__API_KEY"
 fi
-
-#lint
-# (cd app && npm install --silent && npm run lint)
- 
-
-# #test authorize
-# (cd app/authorize && npm install --silent && npm test)
-
-# #test interests
-# (cd app/interests && npm install --silent && npm test)
-
-# #test experiences
-# (cd app/experiences && npm install --silent && npm test)
-
-# #test reviews
-# (cd app/reviews && npm install --silent && npm test)
-
-# #test Trip
-# (cd app/trips && npm install --silent && npm test)
  
