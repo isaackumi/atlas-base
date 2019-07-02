56ccf1896a15c1d9ce337b323f536f201d10960b
isaac kumi <iyzekkummy29@gmail.com>
2019-07-02 10:05:02 +0000

    Add source files

--- src/server.js
+++ src/server.js
@@ -10,16 +10,12 @@ app.use(cors())
 app.use( bodyParser.json( { limit: '50mb' } ) );
 app.use( bodyParser.urlencoded( { limit: '50mb', extended: true } ) );
 
-app.use('/api/v1/', [UserController])
-
 app.use( '*', (req, res, next) => {
     res.sendStatus( '404' );
 });
 
 const server = http.createServer(app);
 
-sequelize.sync({force: false}).then( async () => {
-  server.listen( process.env.PORT || 3000, () => {
-    console.info( `yithrah-api on port ${server.address().port} is online.` );
-  } );
-})
+server.listen( process.env.PORT || 3000, () => {
+  console.info( `waymo-api on port ${server.address().port} is online.` );
+} );
